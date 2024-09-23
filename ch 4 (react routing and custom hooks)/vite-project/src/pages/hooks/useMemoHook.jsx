f5caa60db3e8f0e33189f9370dfc35d4b8fd982c
romail <romailbutt8@gmail.com>
2024-09-23 08:45:36 +0500

    useMemo done

--- ch 4 (react routing and custom hooks)/vite-project/src/pages/hooks/useMemoHook.jsx
+++ ch 4 (react routing and custom hooks)/vite-project/src/pages/hooks/useMemoHook.jsx
@@ -1,35 +1,44 @@
-import React, { useState } from 'react'
-import UseFetch from '../../hooks/useFetch'
+import React, { useMemo, useState } from "react";
+import UseFetch from "../../hooks/useFetch";
 
 const UseMemoHook = () => {
-    const {data , loading} = UseFetch('https://dummyjson.com/products');
-    const [flag, setFlag] = useState(false);
-    function filterProductByPrice(getProduct){
-        console.log(getProduct);
-        
-        return getProduct?.length > 0 ?  getProduct.filter(ele => ele.price > 10) : [];
-    }
+  const { data, loading } = UseFetch("https://dummyjson.com/products");
+  const [flag, setFlag] = useState(false);
+  function filterProductByPrice(getProduct) {
+    console.log('rendering');
 
-    if(loading){
-        return <>
+    return getProduct?.length > 0
+      ? getProduct.filter((ele) => ele.price > 10)
+      : [];
+  }
+  const memorizeVersion = useMemo(() => {
+   return filterProductByPrice(data?.products);
+  }, [data?.products]);
+  if (loading) {
+    return (
+      <>
         <div className="">Loading</div>
-        </>
-    }
-console.log(data?.products);
+      </>
+    );
+  }
 
   return (
     <>
-    <div style={{
-        color: flag? 'green' : 'red'
-    }}>useMemoHook</div>
-    <button onClick={()=> setFlag(!flag)}>Click Me</button>
-    <ul>
-        {
-            filterProductByPrice(data?.products).map((item , key) => <li key={key}>{item.title}</li>)
-        }
-    </ul>
+      <div
+        style={{
+          color: flag ? "green" : "red",
+        }}
+      >
+        useMemoHook
+      </div>
+      <button onClick={() => setFlag(!flag)}>Click Me</button>
+      <ul>
+        {memorizeVersion.map((item, key) => (
+          <li key={key}>{item.title}</li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default UseMemoHook
+export default UseMemoHook;
